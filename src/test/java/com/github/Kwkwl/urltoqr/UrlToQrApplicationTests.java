98377f8f69b37783d567d859da5a1bf6e70fddcc
Kwkwl <yel0129k@gmail.com>
2026-08-01 19:40:36 +0900

    Spring Boot 초기 구성 (#2)

---
+++ src/test/java/com/github/Kwkwl/urltoqr/UrlToQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Kwkwl.urltoqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlToQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
