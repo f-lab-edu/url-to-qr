7bf22d6ae41763723673f9d71636d3d7ce2da771
Kwkwl <yel0129k@gmail.com>
2026-08-09 16:44:41 +0900

    QR 생성 API 구í (#3) - 예ì외 처리 수정

--- src/main/java/com/github/Kwkwl/urltoqr/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/Kwkwl/urltoqr/exception/GlobalExceptionHandler.java
@@ -1,15 +1,10 @@
 package com.github.Kwkwl.urltoqr.exception;
 
-import com.google.zxing.WriterException;
-import org.apache.tomcat.util.http.InvalidParameterException;
-import org.springframework.http.HttpStatus;
+import com.github.Kwkwl.urltoqr.dto.ErrorResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,29 +21,16 @@ public class GlobalExceptionHandler {
                 ));
     }
 
-    @ExceptionHandler(QRGenerationException.class)
-    public ResponseEntity<ErrorResponse> handleQrGeneration(
-            QRGenerationException exception
-    ) {
-        return ResponseEntity.internalServerError()
-                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
-                .body(new ErrorResponse(
-                        "QR_GENERATION_FAILED",
-                        "QR 코드 생성 중 오류가 발생했습니다."
-                ));
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(
             Exception exception
     ) {
-        // log.error("예상하지 못한 오류", exception);
 
         return ResponseEntity.internalServerError()
                 .contentType(MediaType.APPLICATION_PROBLEM_JSON)
                 .body(new ErrorResponse(
                         "INTERNAL_SERVER_ERROR",
-                        "서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."
+                        "QR 코드 생성 중 오류가 발생했습니다."
                 ));
     }
 }
