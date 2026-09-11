5b2fcd33e38158c309213fbe2f41ae59e693cbe6
Kwkwl <yel0129k@gmail.com>
2026-09-11 22:34:01 +0900

    build: 프론트엔드 빌드 환경 개선
    React 및 Docker 베이스 이미지 버전 명시, 멀티스테이지 빌드와 .dockerignore 적용

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -eu
 
-: "${VITE_QR_API_BASE_URL:=http://localhost:8080}"
-export VITE_QR_API_BASE_URL
-
-envsubst \
-  < /opt/runtime-config.template.js \
+envsubst '${VITE_QR_API_BASE_URL}' \
+  < /etc/runtime-config.template.js \
   > /usr/share/nginx/html/runtime-config.js
 
-exec nginx -g "daemon off;"
+exec nginx -g 'daemon off;'
